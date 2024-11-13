43133ecc6c70030b13447c4be09d0b6537ee3e9b
SayfullayevBekzod <sayfullayevbekzod1234@gmail.com>
2024-11-13 15:09:25 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p(9_w6gqjsc-^0edfo_$+$t-uqsdyghfu5k$tii7tf7#e(l$ug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
